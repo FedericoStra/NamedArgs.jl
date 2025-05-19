e4c6a950fb12b18d8281560ed0a5bf09329089b0
Documenter.jl <documenter@juliadocs.github.io>
2025-05-19 15:40:12 +0000

    build based on cb8c85f

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"#NamedArgs","page":"Home","title":"NamedArgs","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for NamedArgs.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+[{"location":"#NamedArgs","page":"Home","title":"NamedArgs","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for NamedArgs.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"#NamedArgs.@na-Tuple{Expr}","page":"Home","title":"NamedArgs.@na","text":"@na f(arg=val, ...; kwargs)\n\nAllow naming arguments in a function call.\n\n\n\n\n\n","category":"macro"}]
 }
