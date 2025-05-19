cf20edc6d1c02288deeb830afcaaf70ee3fbb383
Documenter.jl <documenter@juliadocs.github.io>
2025-05-19 14:15:50 +0000

    build based on 5384a08

---
+++ v0.0.0/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#NamedArgs","page":"Home","title":"NamedArgs","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for NamedArgs.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+}
